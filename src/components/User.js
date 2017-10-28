56afaeb63b6fbbcf767a6131a7813f1cd06f38e5
Shalini Krishan <shalini.krishan@hotmail.com>
2017-10-28 17:52:21 +1000

    Fully selectable.

--- src/components/User.js
+++ src/components/User.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "./WhoIsFree.css";
 
-function User(props) {
 
-  let block = "user-block";
-  let cn = "user-photo";
+class User extends Component {
 
-  if (props.coffeePhoto) {
-    cn = "coffee-photo";
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: false
+    }
+
+    this.toggleSelect = this.toggleSelect.bind(this);
   }
 
-  if (props.selected) {
-    block = "user-block user-selected";
+  toggleSelect() {
+    this.setState({selected: !this.state.selected});
   }
 
-  return (
-    <div className="user-block">
-      <img className={cn} src={props.photo} alt="User"/>
-      <p className="user-name">{props.name}</p>
+  render() {
+    let block = "user-block";
+    let cn = "user-photo";
 
-    </div>
-  );
+    if (this.props.coffeePhoto) {
+      cn = "coffee-photo";
+    }
+
+    if (this.state.selected) {
+      block = "user-block user-selected";
+    }
+
+    return (
+      <div className={block} onClick={this.toggleSelect}>
+        <img className={cn} src={this.props.photo} alt="User"/>
+        <p className="user-name">{this.props.name}</p>
+
+      </div>
+    );
+  }
 }
 
 export default User;
