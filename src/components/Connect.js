9a849137e07bf5cf1265d77dc4335173ffc0c05e
Roy Portas <royportas@gmail.com>
2017-10-28 15:21:46 +1000

    Added connect description

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -5,6 +5,11 @@ class Connect extends Component {
     return (
       <div>
         <h2>Connect with your co-workers!</h2>
+
+        <p style={{width: "80%", margin: "auto"}}>
+          Brew-tiful allows you to connect with your co-workers through
+          the Tanda system, allowing you to form groups with peoples who do the same shifts as you or who work in the same department.
+        </p>
       </div>
     );
   }
