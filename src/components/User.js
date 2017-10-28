cbabe93b6b2b29ad80be28abaad00007670aced5
Shalini Krishan <shalini.krishan@hotmail.com>
2017-10-28 15:23:52 +1000

    Added user.css

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
+import "./User.css";
 
 class User extends Component {
   render() {
     return (
       <div>
-        <p>User page sdfg!</p>
+        <p>Select your avaiablity!</p>
+        <table className="paleBlueRows">
+			<thead>
+			<tr>
+			<th>&nbsp;</th>
+			<th>7am-8am</th>
+			<th>8am-9am</th>
+			<th>9am-10am</th>
+			<th>10am-11am</th>
+			<th>11am-12pm</th>
+			<th>12pm-1pm</th>
+			<th>1pm-2pm</th>
+			<th>2pm-3pm</th>
+			<th>3pm-4pm</th>
+			<th>4pm-5pm</th>
+			<th>5pm-6pm</th>
+			</tr>
+			</thead>
+		
+			<tbody>
+			
+			<tr>
+			<td>Select your avaiablity</td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			<td><label><input type="checkbox" /></label></td>
+			</tr>
+			</tbody>
+			</table>
       </div>
     );
   }
