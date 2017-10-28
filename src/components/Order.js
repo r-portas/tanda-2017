7a236c7cb154258e750e13e03ea3caf05cfb38df
Roy Portas <royportas@gmail.com>
2017-10-28 17:13:37 +1000

    Worked on orders

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 
+import longBlack from "../imgs/coffee/Long Black.png";
+import cap from "../imgs/coffee/Cappucino.png";
+import amer from "../imgs/coffee/Americano.png";
+import espr from "../imgs/coffee/Espresso.png";
+import viena from "../imgs/coffee/Viena.png";
+
 import "./Order.css";
 
 class Order extends Component {
@@ -18,10 +24,34 @@ class Order extends Component {
 				{
 					name: "Lakeside"
 				}
-			]
+      ],
+      coffee: [
+        {
+          name: "Long Black",
+          img: longBlack
+        },
+        {
+          name: "Cappucino",
+          img: cap
+        },
+        {
+          name: "Americano",
+          img: amer
+        },
+        {
+          name: "Espresso",
+          img: espr
+        },
+        {
+          name: "Viena",
+          img: viena
+        },
+
+      ]
     };
 
 		this.renderShops = this.renderShops.bind(this);
+    this.renderCoffee = this.renderCoffee.bind(this);
 
   }
 
@@ -31,12 +61,26 @@ class Order extends Component {
     });
   }
 
+  renderCoffee() {
+    return this.state.coffee.map((coffee) => {
+      return <div className="Order-shop">
+        <p>{coffee.name}</p>
+        <img className="Order-coffee-image" src={coffee.img}/>
+      </div>
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>Order</h2>
 				<div className="Order-shop-container">
 					{ this.renderShops() }
+				</div>
+
+        <h2>Coffee</h2>
+				<div className="Order-shop-container">
+					{ this.renderCoffee() }
 				</div>
 
       </div>
